5e2fc95298df4943e5ae2073d154fc486a7a326c
domingo79 <andria140779@yahoo.it>
2021-06-21 17:37:50 +0200

    mio testo da censurare

---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- puttana x 9, culo x 2, cazzo x 2, merda x 1, sega x 1. -->
+<?php
+$testo = 'Sei carino
+Sei simpatico
+Se vuoi vieni a casa ti presento i miei
+Tu mi presenti i tuoi, non fare la puttana
+Canti alle jam? Non fare la puttana
+Fuggi da me? Non fare la puttana
+Ancora mi cerchi? Non fare la puttana
+Tu speri ci sia? Non fare la puttana
+Di meglio non hai, non fare la puttana
+Chi ti ascolta mai? Non fare la puttana
+Ti guardi alle spalle? Non fare la puttana
+Ti sparano alle palle, non fare la puttana
+Io me ne sbatto della tua recensione
+Intorno al mio disco monti una recinzione
+Io faccio sci nautico sulla tua pozione
+Vorresti vedermi in ebollizione
+Ogni volta che c\'è qualcuno che fa il rap
+Io prendo una fionda e mi lancio a lezioni di step
+Almeno guardo un po\' di culo in vista con paillettes
+Al posto di \'sti culi ciccioni come una crêpe
+Non c\'è…';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+
+<p> <?php var_dump($testo); ?></p>
+
+</body>
+</html>
